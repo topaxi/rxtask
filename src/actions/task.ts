aa45832ca9a9c4299691aeca0ca3bedb7d31aff7
Damian Senn <senn@puzzle.ch>
2018-04-21 20:10:59 +0200

    Fix typo

--- src/actions/task.ts
+++ src/actions/task.ts
@@ -2,7 +2,7 @@ import { ActionWithPayload, createAction } from '../actions'
 import { TaskInstanceWithState } from '../reducers/task'
 
 export const TASK_INSTANCE_STATE_UPDATE_ACTION =
-  '[Task] Task instance statate update'
+  '[Task] Task instance state update'
 
 export type TaskInstanceStateUpdateAction<T> = ActionWithPayload<
   typeof TASK_INSTANCE_STATE_UPDATE_ACTION,
